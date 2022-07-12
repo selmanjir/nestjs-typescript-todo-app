6c72542afcd902c2e831cb5e33693a5fc1939d13
Selman Jir CAN <cselmanjir@gmail.com>
2022-07-12 16:44:32 +0300

    project start

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { TodosService } from './todos.service';
+
+@Controller('todos')
+export class TodosController {
+    constructor(private todosService: TodosService) {}
+
+    @Get()
+    getAllTodos() {
+        return this.todosService.getAllTodos();
+    }
+
+}
---
+++ src/todos/todos.service.ts
@@ -0,0 +1,9 @@
+import { Get, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TodosService {
+    private todos = [];
+    getAllTodos() {
+        return this.todos;
+    }
+}
